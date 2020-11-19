9b1a3fb76fd1706ac6976b77c7b02d4c143f29e8
xa0082249956 <576810415@qq.com>
2020-11-19 20:16:27 +0800

    create action

---
+++ download.ps1
@@ -0,0 +1,20 @@
+#! /usr/bin/env pwsh
+param (
+    [Parameter(Mandatory)]
+    [string]
+    $Repository,
+    [Parameter(Mandatory)]
+    [string]
+    $File,
+    [string]
+    $Version = "latest",
+    [string]
+    $OutPath = "."
+)
+
+$releases = Invoke-RestMethod "https://api.github.com/repos/$Repository/releases/$version"
+$asset = $releases.assets | Where-Object name -EQ $File
+if (-not (Test-Path $OutPath)) {
+    New-Item $OutPath -ItemType Directory
+}
+Invoke-WebRequest -Uri $asset.browser_download_url -OutFile (Join-Path $OutPath $File)
